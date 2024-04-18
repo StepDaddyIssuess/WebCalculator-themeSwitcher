310e527cc5da11f1fead9b41712f442b9db8edb1
Kyano DeBeuckelaere <kyano.debeuckelaere12@gmail.com>
2024-04-18 11:30:38 +0200

    Added themes but not for the slider yet.

---
+++ index.js
@@ -0,0 +1,11 @@
+// range.oninput = function() {
+    //   if (this.value == 1){
+    //     text.innerHTML = "Light";
+    //   }
+    //   else if (this.value == 2){
+    //     text.innerHTML = "Dark";
+    //   }
+    //   else if (this.value == 3){
+    //     text.innerHTML = "purple";
+    //   }
+    // };
