2491dc05be010d7b91a0128e5a7b8c8373f6790e
Kyano DeBeuckelaere <kyano.debeuckelaere12@gmail.com>
2024-04-21 12:32:49 +0200

    Done!

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-// range.oninput = function() {
-    //   if (this.value == 1){
-    //     text.innerHTML = "Light";
-    //   }
-    //   else if (this.value == 2){
-    //     text.innerHTML = "Dark";
-    //   }
-    //   else if (this.value == 3){
-    //     text.innerHTML = "purple";
-    //   }
-    // };
+// show the number that is pressed on the screen
+const outcome = document.querySelector(".outcome");
+function appendDisplay(input){
+  outcome.innerText += input;
+}
+
+function clearDisplay(){
+  outcome.innerText = "";
+}
+
+function calculate(){
+  try{
+    const result = eval(outcome.innerText);
+    outcome.innerText = result;
+  }
+  catch(err){
+    outcome.innerText = "ERROR";
+
+    setTimeout(() => {
+      outcome.innerText = "";
+    }, 2500);
+  }
+}
+
+function del(){
+  outcome.innerText = outcome.innerText.slice(0, -1);
+}
